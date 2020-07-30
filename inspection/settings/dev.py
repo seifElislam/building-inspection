d6328a0f9bc3021b58939543689f4716772ad54f
seifElislam <seif.eleslam1990@gmail.com>
2020-07-30 20:45:34 +0200

    Connect to dev database by env vars

--- inspection/settings/dev.py
+++ inspection/settings/dev.py
@@ -3,4 +3,26 @@ Development Settings
 """
 # pylint: disable=wildcard-import
 # pylint: disable=unused-wildcard-import
+import os
+import environ
 from inspection.settings.base import *
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '../env_vars', '.env_dev'))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USERNAME'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': 3306,
+        'OPTIONS': {
+            # Tell MySQLdb to connect with 'utf8mb4' character set
+            'charset': 'utf8mb4'
+        }
+    }
+}
