57924ddd6f1bdac4fb0d9e64b663a7bf9379a224
seifElislam <seif.eleslam1990@gmail.com>
2020-07-30 23:51:50 +0200

    Add swagger configurations

--- inspection/settings/dev.py
+++ inspection/settings/dev.py
@@ -3,7 +3,7 @@ Development Settings
 """
 # pylint: disable=wildcard-import
 # pylint: disable=unused-wildcard-import
-import os
+from datetime import timedelta
 import environ
 from inspection.settings.base import *
 
@@ -26,3 +26,64 @@ DATABASES = {
         }
     }
 }
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS += [
+    'core.apps.CoreConfig',
+    'rest_framework',
+    'django_filters',
+    'drf_yasg',
+]
+
+LOGIN_URL = 'rest_framework:login'
+LOGOUT_URL = 'rest_framework:logout'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        # 'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=45),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
+
+SWAGGER_URL=''
+
+SWAGGER_SETTINGS = {
+    'DEFAULT_INFO': 'inspection.urls.API_INFO'
+}
+
+
