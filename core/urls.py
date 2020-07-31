ea6ba7793eca26952f7b30a7f53829e0f163446c
seifElislam <seif.eleslam1990@gmail.com>
2020-07-31 07:09:34 +0200

    Add models viewsets

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,26 @@
 from rest_framework.routers import DefaultRouter
-from core.views import UserViewSet
+from core.views import UserViewSet, BuildingViewSet, MetaDataViewSet, DocumentViewSet
 
 USER_ROUTER = DefaultRouter()
 USER_ROUTER.register(r'users', UserViewSet, basename='User')
 
+BUILDING_ROUTER = DefaultRouter()
+BUILDING_ROUTER.register(r'buildings', BuildingViewSet, basename='Building')
+
+METADATA_ROUTER = DefaultRouter()
+METADATA_ROUTER.register(r'metadata', MetaDataViewSet, basename='Metadata')
+
+DOCUMENT_ROUTER = DefaultRouter()
+DOCUMENT_ROUTER.register(r'document', DocumentViewSet, basename='Document')
+
 USERS_URLS = USER_ROUTER.urls
+BUILDINGS_URLS = BUILDING_ROUTER.urls
+METADATA_URLS = METADATA_ROUTER.urls
+DOCUMENT_URLS = DOCUMENT_ROUTER.urls
+
+
 urlpatterns = []
 urlpatterns += USERS_URLS
+urlpatterns += BUILDINGS_URLS
+urlpatterns += METADATA_URLS
+urlpatterns += DOCUMENT_URLS
